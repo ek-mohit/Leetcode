1dd6601cab7a463a84358314e7abaf4481697c8f
Mohit Gangwar <mohitgangwaryyy@gmail.com>
2026-07-09 09:04:36 +0530

    Time: 0 ms (100%), Space: 13.4 MB (55.09%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -36,7 +36,7 @@ public:
 
 }
 */
-int solveTab (vector<int>& days, vector<int>& costs, int index){
+int solveTab (vector<int>& days, vector<int>& costs){
     int n=  days.size();
     vector<int> dp(n+1, INT_MAX);
    
@@ -49,11 +49,11 @@ int solveTab (vector<int>& days, vector<int>& costs, int index){
     int option1 = costs[0] +dp[k+1];
 
     int i;
-    for (i=index ; i<n && days[i] < days[k]+7 ; i++);
+    for (i= k  ; i<n && days[i] < days[k]+7 ; i++);
 
     int option2 = costs[1] + dp[i];
 
-    for (i=index ; i<n && days[i] < days[k]+30 ; i++);
+    for (i= k ; i<n && days[i] < days[k]+30 ; i++);
 
     int option3 = costs[2] + dp[i];
 
@@ -66,7 +66,7 @@ int solveTab (vector<int>& days, vector<int>& costs, int index){
     int mincostTickets(vector<int>& days, vector<int>& costs) {
 
         //vector<int> dp(days.size()+1 , -1);
-        return solveTab(days, costs, 0);
+        return solveTab(days, costs);
         
     }
 };
